86f110c4732518bb66272edaba7a5f47bd610457
ma18015 <ma18015@shibaura-it.ac.jp>
2019-12-17 16:52:12 +0900

    add null check

--- util.sh
+++ util.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+RED="\033[31m%s\033[m"
+GREEN="\033[32m%s\033[m"
 pad=$(printf '%0.1s' "="{1..29})
+
 pad_print(){
   padlength=29
   string=${1}
@@ -20,5 +23,19 @@ print_files(){
       echo " ${failfile}"
     done
     printf "%s\n" "$pad"
+  fi
+}
+
+null_check(){
+  basename=${1}
+  arr=( $(grep -LE "\s*fp\s*==\s*NULL\s*" $basename*.c) )
+  if [ ${#arr[@]} -gt 0 ]; then
+    print_files arr[@] "Not NULL check"
+    return 1
+  else
+    printf "\n$pad\n"
+    printf $GREEN "All files check NULL"
+    printf "\n$pad\n"
+    return 0
   fi
 }
